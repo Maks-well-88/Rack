19f8eb48c148012a1a4761819f67480c4aacb14c
Maksim Pisartsev <Maxwell88@yandex.ru>
2021-10-31 15:40:45 +0300

    Bug fixed: empty request

--- hendler.rb
+++ hendler.rb
@@ -7,29 +7,25 @@ class Hendler
   HEADERS = {'Content-Type' => 'text/plain'}
 
   def choose_response
-    if request_is_valid?
-      response(200, ["#{time_formatter.convert_params_for_response}"])
-    else
+    if time_formatter.params_of_request_is_empty?
+      response(400, ["Missing request parameters"])
+    elsif request_is_invalid?
       response(400, ["Unknown time format #{time_formatter.get_unknown_params}"])
+    else
+      response(200, ["#{time_formatter.convert_params_for_response}"])
     end
   end
 
   def process_the_request(env)
     @time_formatter = TimeFormatter.new
-    time_formatter.get_params_of_request(request(env))
-  end
-
-  private
-
-  def request(env)
-    Rack::Request.new(env)
+    time_formatter.get_params_of_request(Rack::Request.new(env))
   end
 
   def response(status, body)
     Rack::Response.new(body, status, HEADERS).finish
   end
 
-  def request_is_valid?
-    time_formatter.get_unknown_params.empty?
+  def request_is_invalid?
+    time_formatter.get_unknown_params.any?
   end
 end
--- time_formatter.rb
+++ time_formatter.rb
@@ -2,22 +2,32 @@ require 'cgi'
 
 class TimeFormatter
 
+  attr_reader :params_of_request, :time_params
+
   TIME_FORMAT = {
     "year" => "%Y", "month" => "%m", "day" => "%d",
     "hour" => "%H", "minute" => "%M", "second"=> "%S"
   }
 
   def get_params_of_request(request)
-    params_of_request = CGI.parse(request.query_string)
+    @params_of_request = CGI.parse(request.query_string)
+    params_of_request_is_empty? ? @time_params = [] : get_params_for_response
+  end
+
+  def get_params_for_response
     @time_params = params_of_request["format"][0].split(',').map(&:downcase).uniq
   end
 
   def get_unknown_params
-    @unknown_time_params = @time_params - %w[year month day hour minute second]
+    @unknown_time_params = time_params - %w[year month day hour minute second]
   end
 
   def convert_params_for_response
     search_words = /year|month|day|hour|minute|second/
-    Time.now.strftime(@time_params.join('-').gsub(search_words, TIME_FORMAT))
+    Time.now.strftime(time_params.join('-').gsub(search_words, TIME_FORMAT))
+  end
+
+  def params_of_request_is_empty?
+    params_of_request["format"].empty?
   end
 end
